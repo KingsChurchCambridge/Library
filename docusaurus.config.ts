51d3ccb7c91acbe8b21ff12b66735488c39be243
Zavid <zavidegbue@gmail.com>
2024-08-18 00:10:54 +0100

    Set baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -4,21 +4,24 @@ import plugins from './config/plugins';
 import presets from './config/presets';
 import themeConfig from './config/theme';
 
+const organizationName = 'KingsChurchCambridge';
+const projectName = 'Library';
+
 const config: Config = {
   title: "Library | King's Church Cambridge",
   favicon: 'favicon.ico',
   url: process.env.CI
     ? 'https://kingschurchcambridge.github.io/Library/'
     : 'http://localhost:3000',
-  baseUrl: '/',
+  baseUrl: `/${projectName}/`,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
   markdown: { format: 'mdx' },
   plugins,
   presets,
   themeConfig,
-  organizationName: 'KingsChurchCambridge',
-  projectName: 'Library',
+  organizationName,
+  projectName,
   deploymentBranch: 'main',
 };
 
